52e36d33abbb1261525f07ad703753431b586715
陳冠哲 <Jeremy.Chen@want-want.com>
2019-05-13 16:41:02 +0800

    initial again

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import { Switch, Route } from 'react-router-dom'
 import Drawer from './components/drawer'
 import Menu from './components/menu'
 
-import Lesson01 from './components/lesson01'
-import Lesson02 from './components/lesson02'
+import Chapter01 from './chapter/01'
+import Chapter02 from './chapter/02'
 
 const App: React.FC = () => {
   return (
     <Drawer title="React Router" renderMenu={<Menu />}>
       <div className="App">
         <Switch>
-          <Route path="/lesson01" component={Lesson01} />
-          <Route path="/lesson02" component={Lesson02} />
-          <Route path="/" component={Lesson01} exact={true} />
+          <Route path="/lesson01" component={Chapter01} />
+          <Route path="/lesson02" component={Chapter02} />
+          <Route path="/" component={Chapter01} exact={true} />
         </Switch>
       </div>
     </Drawer>
